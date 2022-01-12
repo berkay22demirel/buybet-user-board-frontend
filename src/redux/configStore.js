0b70f673ebf0f33a257b95cf5c1f059b1b0f9fa6
berkay-demirel <berkay_demirel22@hotmail.com>
2022-01-12 14:11:28 +0300

    add state to localstorage

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,18 +1,32 @@
 import { createStore } from "redux";
 import authReducer from "./authReducer";
-
-const defaultAuthState = {
-  isLoggedIn: false,
-  username: undefined,
-  password: undefined,
-};
+import { AUTH_LOCAL_STORAGE_KEY } from "./reduxConstants";
 
 const configStore = () => {
-  return createStore(
+  const buybetAuth = localStorage.getItem(AUTH_LOCAL_STORAGE_KEY);
+
+  let stateInLocalStorage = undefined;
+
+  if (buybetAuth) {
+    try {
+      stateInLocalStorage = JSON.parse(buybetAuth);
+    } catch (error) {}
+  }
+
+  const store = createStore(
     authReducer,
-    defaultAuthState,
+    stateInLocalStorage,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+
+  store.subscribe(() => {
+    localStorage.setItem(
+      AUTH_LOCAL_STORAGE_KEY,
+      JSON.stringify(store.getState())
+    );
+  });
+
+  return store;
 };
 
 export default configStore;
--- src/redux/reduxConstants.js
+++ src/redux/reduxConstants.js
@@ -1,2 +1,4 @@
 export const LOGOUT_SUCCESS = "logout-success";
 export const LOGIN_SUCCESS = "login-success";
+
+export const AUTH_LOCAL_STORAGE_KEY = "buybet-auth";
