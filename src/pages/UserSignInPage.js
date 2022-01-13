642b6ae20a5fb4458578baa237d51ad896b63102
berkay-demirel <berkay_demirel22@hotmail.com>
2022-01-13 18:46:23 +0300

    add hooks to userSignInPage

--- src/pages/UserSignInPage.js
+++ src/pages/UserSignInPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Input from "../components/Input";
 import { withTranslation } from "react-i18next";
 import Button from "../components/Button";
@@ -6,80 +6,71 @@ import { withApiProgress } from "../components/ApiProgress";
 import { connect } from "react-redux";
 import { signInHandler } from "../redux/authActions";
 
-class UserSignInPage extends Component {
-  state = {
-    username: null,
-    password: null,
-    error: null,
-  };
+const UserSignInPage = (props) => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
 
-  onChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-      error: null,
-    });
-  };
+  useEffect(() => {
+    setError(undefined);
+  }, [username, password]);
 
-  onClickSignIn = async (event) => {
+  const onClickSignIn = async (event) => {
     event.preventDefault();
     const loginParams = {
-      username: this.state.username,
-      password: this.state.password,
+      username: username,
+      password: password,
     };
-    this.setState({
-      error: null,
-    });
+    setError(undefined);
     try {
-      await this.props.signIn(loginParams);
-      this.props.history.push("/");
+      await props.signIn(loginParams);
+      props.history.push("/");
     } catch (apiError) {
-      this.setState({
-        error: apiError.response.data.errorMessage,
-      });
+      setError(apiError.response.data.errorMessage);
     }
   };
 
-  render() {
-    const { t, pendingApiCall } = this.props;
-    return (
-      <div className="container-md content-row p-4">
-        <div className="row">
-          <div className="col-sm-12 col-lg-6 offset-lg-3">
-            <div className="card h-100 p-4">
-              <h1 className="text-center">{t("Sign In")}</h1>
-              <form className="m-3">
-                <Input
-                  id="usernameInput"
-                  name="username"
-                  label={t("Username")}
-                  onChange={this.onChange}
-                />
-                <Input
-                  id="passwordInput"
-                  name="password"
-                  label={t("Password")}
-                  type="password"
-                  onChange={this.onChange}
-                />
-                {this.state.error && (
-                  <div className="alert alert-danger">{this.state.error}</div>
-                )}
-                <div className="text-center">
-                  <Button
-                    text="Sign In"
-                    pendingApiCall={pendingApiCall}
-                    onClick={this.onClickSignIn}
-                  ></Button>
-                </div>
-              </form>
-            </div>
+  const { t, pendingApiCall } = props;
+
+  return (
+    <div className="container-md content-row p-4">
+      <div className="row">
+        <div className="col-sm-12 col-lg-6 offset-lg-3">
+          <div className="card h-100 p-4">
+            <h1 className="text-center">{t("Sign In")}</h1>
+            <form className="m-3">
+              <Input
+                id="usernameInput"
+                name="username"
+                label={t("Username")}
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
+              />
+              <Input
+                id="passwordInput"
+                name="password"
+                label={t("Password")}
+                type="password"
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
+              {error && <div className="alert alert-danger">{error}</div>}
+              <div className="text-center">
+                <Button
+                  text="Sign In"
+                  pendingApiCall={pendingApiCall}
+                  onClick={onClickSignIn}
+                ></Button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const UserSignInPageWithApiProgress = withApiProgress(
   UserSignInPage,
